0722eb8c6e9b7db2f0d2bceabf5992bc236f1089
SuperPox <calebkohl@gmail.com>
2021-01-28 13:04:36 -0700

    commit tree 04e46aa7444bb9b87c28640bfb58a44065d87ef5

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,28 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'pry'
+
+class GetRequester
+    #attr_accessor :url
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        arr = []
+        programs = JSON.parse(self.get_response_body)
+        programs.collect do |i|   #{"name"=>"Daniel", "occupation"=>"LG Fridge Salesman"}
+            #i["CONVERTED_DATA"]
+             arr << i
+        end
+        arr
+    end
+end
